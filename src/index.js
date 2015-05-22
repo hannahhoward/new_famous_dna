55fac14e9c422ceac3d90667932975af7e773c19
Hannah Howard <hannah@hannahhoward.net>
2015-05-22 00:06:40 -0700

    Initial commit, just messing around

--- src/index.js
+++ src/index.js
@@ -3,33 +3,69 @@
 // Famous dependencies
 var DOMElement = require('famous/dom-renderables/DOMElement');
 var FamousEngine = require('famous/core/FamousEngine');
+var Mesh = require('famous/webgl-renderables/Mesh');
+var Color = require('famous/utilities/Color');
+var OBJLoader = require('famous/webgl-geometries/OBJLoader');
+var Geometry = require('famous/webgl-geometries/Geometry');
+var Camera = require('famous/components/Camera');
+var PointLight = require('famous/webgl-renderables/lights/PointLight');
 
 // Boilerplate code to make your life easier
 FamousEngine.init();
 
 // Initialize with a scene; then, add a 'node' to the scene root
-var logo = FamousEngine.createScene().addChild();
+var scene = FamousEngine.createScene();
+var camera = new Camera(scene).setDepth(1000);
+var logo = scene.addChild();
+var rotateNode = logo.addChild();
 
-// Create an [image] DOM element providing the logo 'node' with the 'src' path
-new DOMElement(logo, { tagName: 'img' })
-    .setAttribute('src', './images/famous_logo.png');
+rotateNode.setRotation(0,0, 1.66);
+
+var lightNode = scene.addChild()
+.setAlign(0.5, 0.5, 0.5)
+.setPosition(0, 0, 250);
+
+var light = new PointLight(lightNode)
+.setColor(new Color('red'));
+
+OBJLoader.load('obj/dna.obj', function(geometries) {
+    for (var i = 0; i < geometries.length; i++) {
+        var buffers = geometries[i];
+        console.log(buffers);
+        var geometry = new Geometry({
+            buffers: [
+                { name: 'a_pos', data: buffers.vertices, size: 3 },
+                { name: 'a_normals', data: buffers.normals, size: 3 },
+                { name: 'a_texCoords', data: buffers.textureCoords, size: 2},
+                { name: 'indices', data: buffers.indices, size: 1 }
+            ]
+        });
+
+        var pieceNode = rotateNode.addChild();
+        var mesh = new Mesh(pieceNode)
+            .setGeometry(geometry);
+
+    }
+});
+
+
 
 // Chainable API
 logo
     // Set size mode to 'absolute' to use absolute pixel values: (width 250px, height 250px)
     .setSizeMode('absolute', 'absolute', 'absolute')
-    .setAbsoluteSize(250, 250)
+    .setAbsoluteSize(3.5, 3.5, 3.5)
     // Center the 'node' to the parent (the screen, in this instance)
     .setAlign(0.5, 0.5)
     // Set the translational origin to the center of the 'node'
-    .setMountPoint(0.5, 0.5)
+    .setMountPoint(-20,-90,95)
     // Set the rotational origin to the center of the 'node'
-    .setOrigin(0.5, 0.5);
+    .setOrigin(-20, -90, 95);
 
 // Add a spinner component to the logo 'node' that is called, every frame
 var spinner = logo.addComponent({
     onUpdate: function(time) {
-        logo.setRotation(0, time / 1000, 0);
+        logo.setRotation(0, time/1000,  0);
         logo.requestUpdateOnNextTick(spinner);
     }
 });
